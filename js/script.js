cef3158e5f1975d6ecb7766b2fc656db33aaa633
danieleercoli243 <danielercoli@gmail.com>
2025-02-11 15:59:13 +0100

    struggling with fetch

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
-const fetchData = async (baseUri, endpoint) => { 
-    fetch('/data.json').then((response) => {  
+const fetchData = async () => { 
+    // uso il metodo fetch per recuperare i dati dal file json
+     fetch('/data.json').then((response) => {  
         if(!response.ok) return console.log('Oops! Something went wrong.');
         
         return response.json();
       }).then((data) => {
-        console.log(data);
-      });
+        return data
+        console.table(data);
+    });
+    
 };
 
 // recupero gli elementi dal DOM
@@ -14,12 +17,32 @@ const dailyBtn = document.getElementById('daily');
 const weeklyBtn = document.getElementById('weekly');
 const monthlyBtn = document.getElementById('monthly');
 
-// creo le variabili utili
+// creo una variabile d'apoggio
 
 let isCLicked = false;
-const baseUri = 'http://http://127.0.0.1:5500/';
-const endpoint = 'data.json';
+const daily = dailyBtn.innerText.toLowerCase();
+
 
 // provo a fare la chiamata 
 
-fetchData();
+const data = fetchData();
+
+
+// faccio un ciclo su data per stampare le card dinamicamente
+
+data.forEach(d => {
+    console.log(daily);
+    const card = `<div class="card">
+          <div class="card-header"></div>
+          <div class="card-body">
+            <div class="category-icon">
+              <h3>${d.title}</h3>
+              <img src="images/icon-ellipsis.svg" alt="immagine dei tre puntini">
+            </div>
+            <div class="data-prev">
+              <h2>${d.timeframes}</h2>
+              <h4>Last Week - <span>7hrs</span></h4>
+            </div>
+            </div>
+        </div>`
+});
